bffd062567d78b940043d6edf9804b7f6ec84b92
RazenaSaleem <razenasaleem@gmail>
2023-10-04 16:41:35 +0530

    defined newendpointhandler

--- cmd/clusterviz/main.go
+++ cmd/clusterviz/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	logger "github.com/sirupsen/logrus"
-	"clusterviz/internal/pkg/server"
-	"os"
+    "clusterviz/internal/pkg/server"
+    logger "github.com/sirupsen/logrus"
+    "os"
 )
 
 func main() {
-	logger.SetFormatter(&logger.JSONFormatter{})
-	logger.SetReportCaller(true)
-	logger.SetLevel(logger.DebugLevel)
-	logger.SetOutput(os.Stdout)
-	logger.Infof("Server Starting...")
-	s, err := server.New()
-	if err != nil {
-		logger.Fatalf("unable to create the server instance, gerror: %v", err)
-	}
-	logger.Infof("Server initialized successfully...")
-	s.Start()
+    logger.SetFormatter(&logger.JSONFormatter{})
+    logger.SetReportCaller(true)
+    logger.SetLevel(logger.DebugLevel)
+    logger.SetOutput(os.Stdout)
+    logger.Infof("Server Starting...")
+    s, err := server.New()
+    if err != nil {
+        logger.Fatalf("Unable to create the server instance, error: %v", err)
+    }
+    logger.Infof("Server initialized successfully...")
+    s.Start()
 }
